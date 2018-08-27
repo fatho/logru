003709aef661f9d4897146a8edd8aee96b7904f1
Fabian Thorand <f.thorand@gmail.com>
2018-08-27 23:13:49 +0200

    Add Zebra puzzle as benchmark

---
+++ src/bin/zebra.rs
@@ -0,0 +1,9 @@
+extern crate logru;
+
+use logru::zebra;
+
+fn main() {
+    let rules = zebra::puzzle_rules();
+    let mut q = rules.query(vec![zebra::puzzle(zebra::var(0))]);
+    println!("{:?}", q.next())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod unification;
 pub mod logic;
 pub mod union_find;
+pub mod zebra;

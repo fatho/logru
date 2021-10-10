e3f9bab218255eb4eb4058ea04ce2a05a924d31e
Fabian Thorand <f.thorand@gmail.com>
2021-10-10 19:44:29 +0200

    Work towards solution
    But there might be a better way.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+pub mod term;
 pub mod union_find;
 pub mod zebra;
+
+use term::*;
 
 #[derive(Debug)]
 pub struct Universe {
@@ -11,7 +14,10 @@ pub struct Sym(usize);
 
 impl Sym {
     pub fn apply(self, args: Vec<Term>) -> AppTerm {
-        AppTerm { functor: self, args }
+        AppTerm {
+            functor: self,
+            args,
+        }
     }
 }
 
@@ -31,16 +37,26 @@ impl Universe {
     }
 
     pub fn query(&self, goals: Vec<AppTerm>) -> Solver {
+        // determine how many goal variables we need to allocate
+        let max_var = goals
+            .iter()
+            .map(|goal| goal.vars())
+            .flatten()
+            .map(|v| v.0)
+            .max()
+            .unwrap_or(0);
+        // initialize solver
         Solver {
             universe: self,
-            goals,
+            unresolved_goals: goals,
+            resolved_goals: vec![],
+            assignments: vec![None; max_var],
+            operations: vec![],
         }
     }
 
     pub fn new() -> Self {
-        Self {
-            symbols: vec![],
-        }
+        Self { symbols: vec![] }
     }
 }
 
@@ -74,43 +90,6 @@ impl SymInfo {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Term {
-    Var(Var),
-    App(AppTerm),
-}
-
-impl From<Var> for Term {
-    fn from(v: Var) -> Self {
-        Term::Var(v)
-    }
-}
-
-impl From<Sym> for Term {
-    fn from(s: Sym) -> Self {
-        Term::App(s.into())
-    }
-}
-
-impl From<AppTerm> for Term {
-    fn from(at: AppTerm) -> Self {
-        Term::App(at)
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct AppTerm {
-    pub functor: Sym,
-    pub args: Vec<Term>,
-}
-
-impl From<Sym> for AppTerm {
-    fn from(s: Sym) -> Self {
-        Self { functor: s, args: vec![] }
-    }
-}
-
-
-#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Rule {
     head: AppTerm,
     tail: Vec<AppTerm>,
@@ -119,41 +98,172 @@ pub struct Rule {
 impl Rule {
     pub fn fact(pred: Sym, args: Vec<Term>) -> Self {
         Self {
-            head: AppTerm { functor: pred, args},
+            head: AppTerm {
+                functor: pred,
+                args,
+            },
             tail: vec![],
         }
     }
 
     pub fn when(mut self, pred: Sym, args: Vec<Term>) -> Self {
-        self.tail.push(AppTerm { functor: pred, args}.into());
+        self.tail.push(
+            AppTerm {
+                functor: pred,
+                args,
+            }
+            .into(),
+        );
         self
+    }
+
+    pub fn max_var(&self) -> usize {
+        std::iter::once(&self.head)
+            .chain(self.tail.iter())
+            .map(|goal| goal.vars())
+            .flatten()
+            .map(|v| v.0)
+            .max()
+            .unwrap_or(0)
     }
 }
 
 pub struct Solver<'u> {
     universe: &'u Universe,
-    goals: Vec<AppTerm>,
-    //assignments: Vec<Option<Sym>>,
+    unresolved_goals: Vec<AppTerm>,
+    resolved_goals: Vec<ResolvedGoal<'u>>,
+    assignments: Vec<Option<Term>>,
+    operations: Vec<SolverOp>,
+}
+
+enum SolverOp {
+    AssignedVar(Var),
+    AllocatedVars(usize),
+    PushedGoals(usize),
+    Resolved,
+}
+
+struct ResolvedGoal<'u> {
+    goal: AppTerm,
+    alternatives: Vec<&'u Rule>,
 }
 
 enum Step {
     Yield,
-    Skip,
+    Continue,
     Done,
 }
 
 impl<'u> Solver<'u> {
     fn step(&mut self) -> Step {
-        // if let Some(goal) = self.goals.pop() {
+        if let Some(goal) = self.unresolved_goals.pop() {
+            // resolve goal
+            println!("Resolving {:?}", goal);
 
-        //     // resolve goal
+            let goal_sym = &self.universe.symbols[goal.functor.0];
 
+            // store alternatives in reverse order so that we can `pop` and still process
+            // them in the original order
+            let alternatives = goal_sym.definitions.iter().rev().collect::<Vec<_>>();
 
-        //     todo!()
-        // } else {
-        //     todo!("backtrack")
-        // }
-        todo!()
+            self.resolve_next(goal, alternatives)
+        } else {
+            self.backtrack()
+        }
+    }
+
+    fn unify(&mut self, goal_term: &Term, rule_term: &Term) -> bool {
+        todo!("Probably won't work due to how terms are represented")
+    }
+
+    fn unify_app(&mut self, goal_term: &AppTerm, rule_term: &AppTerm) -> bool {
+        if goal_term.functor != rule_term.functor {
+            return false;
+        }
+        if goal_term.args.len() != rule_term.args.len() {
+            return false;
+        }
+
+        goal_term
+            .args
+            .iter()
+            .zip(rule_term.args.iter())
+            .all(|(goal_arg, rule_arg)| self.unify(goal_arg, rule_arg))
+    }
+
+    fn resolve_next(&mut self, goal: AppTerm, mut alternatives: Vec<&'u Rule>) -> Step {
+        while let Some(current) = alternatives.pop() {
+            // tentatively add new operations
+            let checkpoint = self.operations.len();
+
+            self.operations.push(SolverOp::Resolved);
+
+            // add uninstantiated variables for the rule
+            let rule_vars = current.max_var();
+            self.assignments.resize(self.assignments.len() + rule_vars, None);
+            self.operations.push(SolverOp::AllocatedVars(rule_vars));
+
+            if self.unify_app(&goal, &current.head) {
+                // TODO: push new goals
+
+                self.resolved_goals
+                    .push(ResolvedGoal { goal, alternatives });
+
+                if self.unresolved_goals.is_empty() {
+                    // If there are no other goals, we found a solution
+                    return Step::Yield;
+                } else {
+                    // Otherwise we need to continue
+                    return Step::Continue;
+                }
+            } else {
+                // restricted backtracking of the unification operations
+                for op in self.operations.drain(checkpoint..).rev() {
+                    match op {
+                        SolverOp::AssignedVar(v) => self.assignments[v.0] = None,
+                        SolverOp::AllocatedVars(num_vars) => {
+                            self.assignments.truncate(self.assignments.len() - num_vars)
+                        }
+                        SolverOp::PushedGoals(_) => (),
+                        // no need to do anything since we didn't actually push it yet
+                        SolverOp::Resolved => (),
+                    }
+                }
+            }
+        }
+
+        // If none of the alternatives matched, put the unresolved goal back and backtrack to
+        // earlier decision.
+        self.unresolved_goals.push(goal);
+        self.backtrack()
+    }
+
+    fn backtrack(&mut self) -> Step {
+        // pop from the backtracking stack until we can try another alternative
+        while let Some(op) = self.operations.pop() {
+            match op {
+                SolverOp::AssignedVar(var) => {
+                    self.assignments[var.0] = None;
+                }
+                SolverOp::AllocatedVars(num_vars) => {
+                    self.assignments.truncate(self.assignments.len() - num_vars);
+                }
+                SolverOp::PushedGoals(num_goals) => {
+                    self.unresolved_goals
+                        .truncate(self.unresolved_goals.len() - num_goals);
+                }
+                SolverOp::Resolved => {
+                    // try the next alternative in the goal resolution queue
+                    let rgoal = self
+                        .resolved_goals
+                        .pop()
+                        .expect("must have resolved goal when backtracking `Resolved`");
+                    self.resolve_next(rgoal.goal, rgoal.alternatives);
+                }
+            }
+        }
+        // Nothing more to scan through
+        Step::Done
     }
 }
 
@@ -163,8 +273,8 @@ impl<'u> Iterator for Solver<'u> {
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             match self.step() {
-                Step::Yield => todo!(),
-                Step::Skip => continue,
+                Step::Yield => break Some(()),
+                Step::Continue => continue,
                 Step::Done => break None,
             }
         }

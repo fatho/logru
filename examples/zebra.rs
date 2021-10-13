57126b14bc030cdb683e612adc50d9c32c0cbb99
Fabian Thorand <f.thorand@gmail.com>
2021-10-13 18:40:03 +0200

    Take number of repetitions in zebra example
    This makes it an easier target for the profiler.

--- examples/zebra.rs
+++ examples/zebra.rs
@@ -1,20 +1,27 @@
 use std::time::Instant;
 
 fn main() {
+    let repeats = std::env::args()
+        .nth(1)
+        .and_then(|arg| arg.parse().ok())
+        .unwrap_or(1);
+
     let mut u = logru::textual::TextualUniverse::new();
     u.load_str(include_str!("../testfiles/zebra.lru")).unwrap();
 
-    let mut solutions = u.query_dfs("puzzle($0).").unwrap();
-    let before = Instant::now();
-    let solution = solutions.next().unwrap();
-    let duration = before.elapsed();
+    for _ in 0..repeats {
+        let mut solutions = u.query_dfs("puzzle($0).").unwrap();
+        let before = Instant::now();
+        let solution = solutions.next().unwrap();
+        let duration = before.elapsed();
 
-    for var in solution {
-        if let Some(term) = var {
-            println!("{}", u.pretty().term_to_string(&term));
-        } else {
-            println!("<bug: no solution>");
+        for var in solution {
+            if let Some(term) = var {
+                println!("{}", u.pretty().term_to_string(&term));
+            } else {
+                println!("<bug: no solution>");
+            }
         }
+        println!("Took {:.3}s", duration.as_secs_f64());
     }
-    println!("Took {:.3}s", duration.as_secs_f64());
 }

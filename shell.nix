1cd5e5acecd21a3f5ed7ee4bc6aa4b96b115f88c
Fabian Thorand <f.thorand@gmail.com>
2024-03-29 13:50:00 +0100

    Update dependencies

--- shell.nix
+++ shell.nix
@@ -1,17 +1,27 @@
 let
   sources = import ./nix/sources.nix;
-  nixpkgs = import sources.nixpkgs {};
+  mozilla = import (sources.nixpkgs-mozilla + "/rust-overlay.nix");
+  nixpkgs = import sources.nixpkgs {
+    overlays = [mozilla];
+  };
+  channel = nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; };
 in
   nixpkgs.mkShell {
-    name = "auto-dev";
+    name = "logru-dev";
+
     nativeBuildInputs = with nixpkgs; [
-      # Rust development
-      rustc
-      cargo
-      rustfmt
-      clippy
+      # Rust core
+      channel.rust
+
+      # Toolchain
+      lld_13
+      lldb_13
+      pkg-config
+
+      # Neat helper tools
       cargo-audit
       cargo-criterion
+      cargo-deny
       cargo-edit
       cargo-flamegraph
       gnuplot  # for criterion
@@ -19,9 +29,10 @@ in
       # Nix tools
       niv
     ];
+
     # Always enable rust backtraces in development shell
     RUST_BACKTRACE = "1";
 
     # Provide sources for rust-analyzer, because nixpkgs rustc doesn't include them in the sysroot
-    RUST_SRC_PATH = "${nixpkgs.rustPlatform.rustLibSrc}";
+    RUST_SRC_PATH = "${channel.rust-src}/lib/rustlib/src/rust/library";
   }

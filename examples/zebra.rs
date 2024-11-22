732385b66f70cfc45685f7557047618defe17714
Fabian Thorand <f.thorand@gmail.com>
2024-11-23 00:35:58 +0100

    Make zebra example more profiling friendly
    When repeating runs, only the first run produces output.

--- examples/zebra.rs
+++ examples/zebra.rs
@@ -10,25 +10,29 @@ fn main() {
     u.load_str(include_str!("../testfiles/zebra.lru")).unwrap();
 
     let query = u.prepare_query("puzzle(Houses).").unwrap();
-    for _ in 0..repeats {
+    for i in 0..repeats {
         let search = logru::query_dfs(u.resolver(), &query);
         let before = Instant::now();
         let solutions = search.collect::<Vec<_>>();
         let duration = before.elapsed();
 
         for solution in solutions.iter() {
-            for var in solution {
-                if let Some(term) = var {
-                    println!("{}", u.pretty().term_to_string(term, query.scope.as_ref()));
-                } else {
-                    println!("<bug: no solution>");
+            if i == 0 {
+                for var in solution {
+                    if let Some(term) = var {
+                        println!("{}", u.pretty().term_to_string(term, query.scope.as_ref()));
+                    } else {
+                        println!("<bug: no solution>");
+                    }
                 }
             }
         }
-        println!(
-            "Took {:.3}s with {} solutions",
-            duration.as_secs_f64(),
-            solutions.len()
-        );
+        if i == 0 {
+            println!(
+                "Took {:.3}s with {} solutions",
+                duration.as_secs_f64(),
+                solutions.len()
+            );
+        }
     }
 }
